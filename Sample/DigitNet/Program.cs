9876b86dc6b302e03bd81d72fb24b271dbb50968
sethflowers <seth.r.flowers@gmail.com>
2013-07-21 16:50:30 -0400

    Added start of Sample Digit OCR project. Loading training data into collection.

---
+++ Sample/DigitNet/Program.cs
@@ -0,0 +1,62 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Seth Flowers">
+//     All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DigitNet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// A demonstration of using neural networks for digit classification (OCR).
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point into the demonstration.
+        /// </summary>
+        public static void Main()
+        {
+            List<TrainingData> trainingDataCollection = LoadTrainingData();
+
+            Console.WriteLine(trainingDataCollection.Count);
+            Console.WriteLine(trainingDataCollection[0].Digit);
+
+            Console.WriteLine("Done - hit enter to continue.");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Loads all the training data from a file.
+        /// </summary>
+        /// <returns>Returns a collection of the training data.</returns>
+        private static List<TrainingData> LoadTrainingData()
+        {
+            List<TrainingData> trainingDataCollection = new List<TrainingData>();
+
+            bool skipLine = true;
+
+            foreach (string data in File.ReadAllLines("../../Files/train.csv"))
+            {
+                if (skipLine)
+                {
+                    skipLine = false;
+                    continue;
+                }
+
+                TrainingData trainingData = new TrainingData();
+                trainingData.Digit = (short)(data[0] - (int)'0');
+
+                string[] lineData = data.Split(",".ToCharArray());
+                trainingData.Data = lineData.Skip(1).Select(s => byte.Parse(s)).ToArray();
+
+                trainingDataCollection.Add(trainingData);
+            }
+
+            return trainingDataCollection;
+        }
+    }
+}
---
+++ Sample/DigitNet/TrainingData.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TrainingData.cs" company="Seth Flowers">
+//     All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DigitNet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Data used to train the neural nets.
+    /// </summary>
+    internal class TrainingData
+    {
+        /// <summary>
+        /// Gets or sets the digit that is represented by the Data array.
+        /// </summary>
+        /// <value>
+        /// The digit represented by the Data array.
+        /// </value>
+        public short Digit { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the image data that will be run through the neural nets.
+        /// </summary>
+        /// <value>
+        /// The image data for a single digit.
+        /// </value>
+        public byte[] Data { get; set; }
+    }
+}
